a195d0fd17f77c913b7c10c567878818874c9786
Tom Tillistrand <tomtillistrand@gmail.com>
2022-04-07 22:05:32 -0400

    Add image optimization task

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,14 @@ const { parallel, watch } = require('gulp');
 
 // Tasks
 const sass = require('./gulp-tasks/sass');
-const fonts = require('./gulp-tasks/fonts.js');
+const fonts = require('./gulp-tasks/fonts');
+const images = require('./gulp-tasks/images');
 
 const watcher = () => {
   watch('./src/scss/**/*.scss', { ignoreInitial: true }, sass);
+  watch('./src/images/**/*', { ignoreInitial: true }, images);
 };
 
-exports.default = parallel(fonts, sass);
+exports.default = parallel(fonts, images, sass);
 
 exports.watch = watcher;
