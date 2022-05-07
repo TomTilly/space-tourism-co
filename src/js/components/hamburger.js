ebe33dad4b378278c36b98d56cc6cd0765dc7876
Tom Tillistrand <tomtillistrand@gmail.com>
2022-05-07 17:07:05 -0400

    Refactor header/nav

--- src/js/components/hamburger.js
+++ src/js/components/hamburger.js
@@ -2,12 +2,12 @@ const hamburger = document.querySelector(
   '.hamburger[aria-controls="nav-list"]'
 );
 
-hamburger.addEventListener('click', (e) => {
+hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('is-active');
   const isExpanded = hamburger.getAttribute('aria-expanded') === 'true';
   hamburger.setAttribute('aria-expanded', !isExpanded);
   document.documentElement.classList.toggle('showing-mobile-nav');
   document
-    .querySelector('.header__list')
-    .classList.toggle('header__list--expanded');
+    .querySelector('.header__nav-list')
+    .classList.toggle('header__nav-list--expanded');
 });
